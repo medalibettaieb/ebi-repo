914cdd1bc5129553e7a5f4d8b3c0376f66a2643d
daly <daly@ESPRIT>
2015-09-30 18:50:16 +0200

    first project
    Signed-off-by: daly <daly@ESPRIT>

---
+++ ebi/src/main/java/entities/Employee.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Employee {
+	private Integer id;
+	private String name;
+
+	public Employee() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
